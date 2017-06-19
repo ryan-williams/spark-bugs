820a8152ad57f4a6a0ad64e9b1afbc142f33497f
Ryan Williams <ryan.blake.williams@gmail.com>
2017-06-19 16:06:39 +0000

    fix test

--- build.sbt
+++ build.sbt
@@ -1,6 +1,7 @@
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.1.0",
+  "org.apache.spark" %% "spark-core" % "2.1.1",
   // Having this on the classpath causes Spark to fail LargeBlockTest.
-  "io.netty" % "netty-all" % "4.1.6.Final"
+  "io.netty" % "netty-all" % "4.1.6.Final",
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
 )
--- src/test/scala/com/foo/LargeBlockTest.scala
+++ src/test/scala/com/foo/LargeBlockTest.scala
@@ -11,13 +11,19 @@ class LargeBlockTest
   extends FunSuite {
 
   /**
+   * [0,1032] pass, [1033,∞) fail.
+   *
+   * 1032 * [1000 bytes + C] is right around what we'd expect if a ≥1MB (1048576 bytes) block-size caused the failure.
+   */
+  for { n ← 1030 to 1035 } {
+    check(n)
+  }
+
+  /**
    * Run a test case that collects `n` 1000-element [[Array]]s of [[Byte]]s – from one partition – to the driver.
    */
   def check(n: Int): Unit = {
     test(s"$n") {
-      val conf = new SparkConf
-      conf.set("spark.master", "local[4]")
-      conf.set("spark.app.name", "FooTest")
 
       val sc = new SparkContext(conf)
 
@@ -38,12 +44,8 @@ class LargeBlockTest
     }
   }
 
-  /**
-   * [0,1032] pass, [1033,∞) fail.
-   *
-   * 1032 * [1000 bytes + C] is right around what we'd expect if a ≥1MB (1048576 bytes) block-size caused the failure.
-   */
-  for { n ← 1030 to 1035 } {
-    test(s"$n") { check(n) }
-  }
+  val conf =
+    new SparkConf()
+      .set("spark.master", "local[4]")
+      .set("spark.app.name", "LargeBlockTest")
 }
